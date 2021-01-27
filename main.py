c07374d63e292d30e301343f8ebf397a74ae4b86
Abhinav Rajesh <abhinavrajesh49@gmail.com>
2021-01-27 17:36:06 +0530

    Changed from normal messages to embeded messages, also pings the class accordingly

--- main.py
+++ main.py
@@ -1,55 +1,100 @@
 import discord
+from discord.ext import commands
+from discord.utils import get
 import os
 import datetime as dt
 import pytz
 from timetable import timetable
 
-client = discord.Client()
+client = commands.Bot(command_prefix="?")
+
+days = {
+    0: 'Monday',
+    1: 'Tuesday',
+    2: 'Wednesday',
+    3: 'Thursday',
+    4: 'Friday',
+    5: 'Saturday',
+    6: 'Sunday',
+}
 
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user.name}')
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='?class'))
 
 @client.event
-async def on_message(message):
-    if message.author == client.user:
+async def on_message(ctx):
+    if ctx.author == client.user:
         return
-    if message.content.startswith('$class'):
-        args_len = len(message.content.split())
+    if ctx.content.startswith('?class'):
+        args_len = len(ctx.content.split())
         now = dt.datetime.now(pytz.timezone('Asia/Kolkata'))
         if args_len == 1:
             if now.weekday() < 5:
                 first = True
                 for i in timetable[now.weekday()]:
-                    text = ''
+                    className = ''
                     if first:
                         first = False
-                        text = '**CS A** \n'
+                        className = 'CS-A'
+                        await ctx.channel.send(get(ctx.guild.roles, name="CS-A").mention)
                     else:
-                        text = '**CS B** \n'
+                        className = 'CS-B'
+                        await ctx.channel.send(get(ctx.guild.roles, name="CS-B").mention)
+                    embed = discord.Embed(title=f"Timetable {className}", description=f"{className} Timetable for {days[now.weekday()]}", color=discord.Color.green())
                     for j in i:
-                        text += j + '\n'
-                    await message.channel.send(text)
+                        temp = j.split(' ') # CVPDE Class at 9:00 AM, MEET LINK: N/A
+                        embed.add_field(name="Subject", value=f"{temp[0]} {temp[1]}", inline=True)
+                        embed.add_field(name="Time", value=temp[3], inline=True)
+                        embed.add_field(name="Meet Link", value=temp[-1], inline=True)
+                    embed.set_thumbnail(url= client.user.avatar_url)
+                    embed.set_footer(icon_url = ctx.author.avatar_url, text=f'Requested by {ctx.author}')
+                    await ctx.channel.send(embed=embed)
             else:
-                await message.channel.send("**No Class today according to Timetable**")
+                embed = discord.Embed(title="No Class", description=f"According to timetable {days[now.weekday()]} is a holiday")
+                embed.set_thumbnail(url= client.user.avatar_url)
+                embed.set_footer(icon_url = ctx.author.avatar_url, text=f'Requested by {ctx.author}')
+                await ctx.channel.send(embed=embed)
         elif args_len == 2:
-            if 'help' == message.content.split(' ')[1].lower():
-                return await message.channel.send("""
-`$class` : Returns timetable for that particular day for both the batches [ CS-A and CS-B],
-`$class ARGS`: Returns the timetable for the batch specified as ARGS
-Currently Accepted `ARGS`:
-    - `CSA` [ Case insensitive ]
-    - `CSB` [ Case insensitive ]
-    - `HELP` [ Case insensitive ]
-""")
-            if 'csa' == message.content.split(' ')[1].lower():
-                text = '**CS A** \n'
-                i = timetable[now.weekday()][0]
-            elif 'csb' == message.content.split(' ')[1].lower():
-                text = '**CS B** \n'
-                i = timetable[now.weekday()][1]
-            for j in i:
-                text += j + '\n'
-            await message.channel.send(text)
+            if 'help' == ctx.content.split(' ')[1].lower():
+                embed = discord.Embed(title = "Commands" ,description = "Commands for the CS2K23 Timetable Reminder Bot", color=discord.Color.red())
+                embed.add_field(name = "?class", value="This command returns the class timetable for both CS-A and CS-B Batch for that particular day", inline=False)
+                embed.add_field(name = "?class csa", value="This command returns the class timetable for CS-A Batch for that particular day", inline=False)
+                embed.add_field(name = "?class csb", value="This command returns the class timetable for CS-B Batch for that particular day", inline=False)
+                embed.set_thumbnail(url= client.user.avatar_url)
+                embed.set_footer(icon_url = ctx.author.avatar_url, text=f'Requested by {ctx.author}')
+                return await ctx.channel.send(embed=embed)
+            if now.weekday() < 5:
+                if 'csa' == ctx.content.split(' ')[1].lower():
+                    className = 'CS-A'
+                    await ctx.channel.send(get(ctx.guild.roles, name="CS-A").mention)
+                    i = timetable[now.weekday()][0]
+                elif 'csb' == ctx.content.split(' ')[1].lower():
+                    className = 'CS-B'
+                    await ctx.channel.send(get(ctx.guild.roles, name="CS-B").mention)
+                    i = timetable[now.weekday()][1]
+                else:
+                    embed = discord.Embed(title = "Commands" ,description = "Commands for the CS2K23 Timetable Reminder Bot", color=discord.Color.red())
+                    embed.add_field(name = "?class", value="This command returns the class timetable for both CS-A and CS-B Batch for that particular day", inline=False)
+                    embed.add_field(name = "?class csa", value="This command returns the class timetable for CS-A Batch for that particular day", inline=False)
+                    embed.add_field(name = "?class csb", value="This command returns the class timetable for CS-B Batch for that particular day", inline=False)
+                    embed.set_thumbnail(url= client.user.avatar_url)
+                    embed.set_footer(icon_url = ctx.author.avatar_url, text=f'Requested by {ctx.author}')
+                    return await ctx.channel.send(embed=embed)
+                embed = discord.Embed(title=f"Timetable {className}", description=f"{className} Timetable for {days[now.weekday()]}", color=discord.Color.green())
+                for j in i:
+                    temp = j.split(' ') # CVPDE Class at 9:00 AM, MEET LINK: N/A
+                    embed.add_field(name="Subject", value=f"{temp[0]} {temp[1]}", inline=True)
+                    embed.add_field(name="Time", value=temp[3], inline=True)
+                    embed.add_field(name="Meet Link", value=temp[-1], inline=True)
+                embed.set_thumbnail(url= client.user.avatar_url)
+                embed.set_footer(icon_url = ctx.author.avatar_url, text=f'Requested by {ctx.author}')
+            else:
+                embed = discord.Embed(title="No Class", description=f"According to timetable {days[now.weekday()]} is a holiday")
+                embed.set_thumbnail(url= client.user.avatar_url)
+                embed.set_footer(icon_url = ctx.author.avatar_url, text=f'Requested by {ctx.author}')
+            await ctx.channel.send(embed=embed)
 
 client.run(os.getenv('TOKEN'))
+
