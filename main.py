1a8402c2ebab1c29d2cc0612cf71a2b006841de0
Abhinav Rajesh <abhinavrajesh49@gmail.com>
2021-01-27 15:45:14 +0530

    Seperated timetable to different py file

--- main.py
+++ main.py
@@ -2,71 +2,9 @@ import discord
 import os
 import datetime as dt
 import pytz
+from timetable import timetable
 
 client = discord.Client()
-
-timetable = {
-    0: [
-            [
-                'CVPDE Class at 9:00 AM, MEET LINK: N/A', 
-                'ALC Class at 10:45 AM, MEET LINK: https://meet.google.com/msx-ppdx-ytx',
-                'DBMS/DS Lab at 01:00 PM, MEET LINK: N/A' 
-            ],
-            [
-                'ALC Class at 9:00 AM, MEET LINK: https://meet.google.com/vok-hnsw-mrx',
-                'CVPDE Class at 10:45 AM, MEET LINK: N/A',
-                'DBMS/DS Lab Class at 01:00 PM, MEET LINK: N/A',
-            ]
-        ],
-    1: [
-            [
-                'DBMS Class at 9:00 AM, MEET LINK: N/A', 
-                'CAO Class at 10:45 AM, MEET LINK: N/A',
-                'MP Class at 01:00 PM, MEET LINK: N/A' 
-            ],
-            [
-                'MP Class at 9:00 AM, MEET LINK: N/A',
-                'CAO Class at 10:45 AM, MEET LINK: N/A',
-                'DBMS Class at 01:00 PM, MEET LINK: N/A',
-            ]
-        ],
-    2: [
-            [
-                'DSA Class at 9:00 AM, MEET LINK: N/A', 
-                'MP Class at 10:45 AM, MEET LINK: N/A',
-                'DBMS/DS Lab at 01:00 PM, MEET LINK: N/A' 
-            ],
-            [
-                'MP Class at 9:00 AM, MEET LINK: N/A',
-                'DSA Class at 10:45 AM, MEET LINK: N/A',
-                'UHV2 Class at 01:00 PM, MEET LINK: N/A',
-            ]
-        ],
-    3: [
-            [
-                'CVPDE Class at 9:00 AM, MEET LINK: N/A', 
-                'CAO Class at 10:45 AM, MEET LINK: N/A',
-                'DSA Class at 01:00 PM, MEET LINK: N/A' 
-            ],
-            [
-                'DSA Class at 9:00 AM, MEET LINK: N/A',
-                'CAO Class at 10:45 AM, MEET LINK: N/A',
-                'CVPDE Class at 01:00 PM, MEET LINK: N/A',
-            ]
-        ],
-    4: [
-            [
-                'ALC Class at 9:00 AM, MEET LINK: https://meet.google.com/msx-ppdx-ytx', 
-                'DBMS Class at 10:45 AM, MEET LINK: N/A',
-                'UHV2 Class at 01:00 PM, MEET LINK: N/A' 
-            ],
-            [
-                'DBMS Class at 9:00 AM, MEET LINK: N/A',
-                'ALC Class at 10:45 AM, MEET LINK: https://meet.google.com/vok-hnsw-mrx',
-                'DBMS/DS Lab at 01:00 PM, MEET LINK: N/A',
-            ]
-        ]
-}
 
 @client.event
 async def on_ready():
@@ -95,6 +33,15 @@ async def on_message(message):
             else:
                 await message.channel.send("**No Class today according to Timetable**")
         elif args_len == 2:
+            if 'help' == message.content.split(' ')[1].lower():
+                return await message.channel.send("""
+`$class` : Returns timetable for that particular day for both the batches [ CS-A and CS-B],
+`$class ARGS`: Returns the timetable for the batch specified as ARGS
+Currently Accepted `ARGS`:
+    - `CSA` [ Case insensitive ]
+    - `CSB` [ Case insensitive ]
+    - `HELP` [ Case insensitive ]
+""")
             if 'csa' == message.content.split(' ')[1].lower():
                 text = '**CS A** \n'
                 i = timetable[now.weekday()][0]
---
+++ timetable.py
@@ -0,0 +1,66 @@
+timetable = {
+    0: [
+            [
+                'CVPDE Class at 9:00 AM, MEET LINK: N/A', 
+                'ALC Class at 10:45 AM, MEET LINK: https://meet.google.com/msx-ppdx-ytx',
+                'DBMS/DS Lab at 01:00 PM, MEET LINK: N/A' 
+            ],
+            [
+                'ALC Class at 9:00 AM, MEET LINK: https://meet.google.com/vok-hnsw-mrx',
+                'CVPDE Class at 10:45 AM, MEET LINK: N/A',
+                'DBMS/DS Lab Class at 01:00 PM, MEET LINK: N/A',
+            ]
+        ],
+    1: [
+            [
+                'DBMS Class at 9:00 AM, MEET LINK: N/A', 
+                'CAO Class at 10:45 AM, MEET LINK: N/A',
+                'MP Class at 01:00 PM, MEET LINK: N/A' 
+            ],
+            [
+                'MP Class at 9:00 AM, MEET LINK: N/A',
+                'CAO Class at 10:45 AM, MEET LINK: N/A',
+                'DBMS Class at 01:00 PM, MEET LINK: N/A',
+            ]
+        ],
+    2: [
+            [
+                'DSA Class at 9:00 AM, MEET LINK: N/A', 
+                'MP Class at 10:45 AM, MEET LINK: N/A',
+                'DBMS/DS Lab at 01:00 PM, MEET LINK: N/A' 
+            ],
+            [
+                'MP Class at 9:00 AM, MEET LINK: N/A',
+                'DSA Class at 10:45 AM, MEET LINK: N/A',
+                'UHV2 Class at 01:00 PM, MEET LINK: N/A',
+            ]
+        ],
+    3: [
+            [
+                'CVPDE Class at 9:00 AM, MEET LINK: N/A', 
+                'CAO Class at 10:45 AM, MEET LINK: N/A',
+                'DSA Class at 01:00 PM, MEET LINK: N/A' 
+            ],
+            [
+                'DSA Class at 9:00 AM, MEET LINK: N/A',
+                'CAO Class at 10:45 AM, MEET LINK: N/A',
+                'CVPDE Class at 01:00 PM, MEET LINK: N/A',
+            ]
+        ],
+    4: [
+            [
+                'ALC Class at 9:00 AM, MEET LINK: https://meet.google.com/msx-ppdx-ytx', 
+                'DBMS Class at 10:45 AM, MEET LINK: N/A',
+                'UHV2 Class at 01:00 PM, MEET LINK: N/A' 
+            ],
+            [
+                'DBMS Class at 9:00 AM, MEET LINK: N/A',
+                'ALC Class at 10:45 AM, MEET LINK: https://meet.google.com/vok-hnsw-mrx',
+                'DBMS/DS Lab at 01:00 PM, MEET LINK: N/A',
+            ]
+        ]
+}
+
+
+
+
